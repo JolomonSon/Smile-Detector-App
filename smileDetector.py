9bd251c5e2d7c8942c1c32dd062498bdc06b0783
John Solomon <jolomonson@gmail.com>
2021-04-10 04:09:54 +0100

    Resuming Application[Not quite understanding tho]

--- smileDetector.py
+++ smileDetector.py
@@ -12,10 +12,13 @@ while True:
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     trainedFaceCoordinates = faceClassifier.detectMultiScale(grayFrame, scaleFactor=1.7, minNeighbors=20)
     trainedSmileCoordinates = smileClassifier.detectMultiScale(grayFrame, scaleFactor=1.7, minNeighbors=20)
-    trainedFaceFrame = [cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,0),4) for (x,y,w,h) in trainedFaceCoordinates]
-    trainedSmileFrame = [cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,255)) for (x, y, w, h) in trainedSmileCoordinates]
     print('Face Coordinate',trainedFaceCoordinates)
-    print('Smile Coordinate',trainedSmileCoordinates)
+    trainedFaceFrame = [cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256),randrange(256),randrange(256))) for (x,y,w,h) in trainedFaceCoordinates]
+    trainedSmileFrame = [cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)),2) for (x, y, w, h) in trainedSmileCoordinates]
+    #theFace = (x,y,w,h)
+    for (x_,y_,w_,h_) in trainedFaceCoordinates :
+        print('Smile Coordinate',trainedSmileCoordinates)
+        cv2.rectangle(frame,(x_,y_),(x_+w_,y_+h_), (randrange(256), randrange(256), randrange(256)))
     cv2.imshow('Smiley Face', frame)
     cv2.waitKey(1)
 
