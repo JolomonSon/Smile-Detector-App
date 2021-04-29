fb809cf8485c96c4ad784cf537c24c298cd324fd
John Solomon <jolomonson@gmail.com>
2021-04-29 02:26:06 +0100

    Labelling the face (Smiling or Not Smiling) [Partly Completed]

--- smileDetector.py
+++ smileDetector.py
@@ -18,12 +18,17 @@ while True:
     # Rectangles on the face
     #for (x,y,w,h) in faceCoordinates:
     faceFrame = [cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256), randrange(256), randrange(256))) for (x,y,w,h) in faceCoordinates]
-    smileFrame = [cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256), randrange(256), randrange(256)),2) for (x,y,w,h) in smileCoordinates]
-    # Get the sub frame (using numpy N-dimensional array slicing)
-    #theFace = frame[y:y+h, x:x+w]
+    #smileFrame = [cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256), randrange(256), randrange(256)),2) for (x,y,w,h) in smileCoordinates]
+    for (x,y,w,h) in smileCoordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)),2)
+    # Label the face (Smiling or Not Smiling)
+        if len(smileCoordinates) > 0:
+            cv2.putText(frame, 'Smiling', (x, y+h+40), fontScale=1, fontFace=cv2.FONT_HERSHEY_PLAIN, color=(256,0,256))
+        else:
+            cv2.putText(frame, 'Not Smiling', (x, y+h+40), fontScale=2, fontFace=cv2.FONT_HERSHEY_PLAIN, color=(0,256,0))
     cv2.imshow('Webcam Smiley Face Detection',frame)
     key = cv2.waitKey(1)
-    #Stop if Q is pressed
+    # Stop if Q is pressed
     if key==81 or key==113:
         break
 #Release the VideoCapture object
