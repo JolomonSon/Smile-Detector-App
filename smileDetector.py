fe0b666259ef5918f44a9f0aee941ef544741583
John Solomon <jolomonson@gmail.com>
2021-04-29 02:17:17 +0100

    SmileyFaceDetection

--- smileDetector.py
+++ smileDetector.py
@@ -1,28 +1,31 @@
 import cv2
 from random import randrange
-
-video = cv2.VideoCapture(0)
-#Face and Smile Classifier
+# Real Time Image(frame)
+video  = cv2.VideoCapture(0)
+# Face and Smile Classifiers
 faceClassifier = cv2.CascadeClassifier('frontalFaceDetector.xml')
 smileClassifier = cv2.CascadeClassifier('smileDetector.xml')
 while True:
     readSuccessful, frame = video.read()
     if not readSuccessful:
         break
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    trainedFaceCoordinates = faceClassifier.detectMultiScale(grayFrame, scaleFactor=1.7, minNeighbors=20)
-    trainedSmileCoordinates = smileClassifier.detectMultiScale(grayFrame, scaleFactor=1.7, minNeighbors=20)
-    print('Face Coordinate',trainedFaceCoordinates)
-    trainedFaceFrame = [cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256),randrange(256),randrange(256))) for (x,y,w,h) in trainedFaceCoordinates]
-    trainedSmileFrame = [cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)),2) for (x, y, w, h) in trainedSmileCoordinates]
-    #theFace = (x,y,w,h)
-    for (x_,y_,w_,h_) in trainedFaceCoordinates :
-        print('Smile Coordinate',trainedSmileCoordinates)
-        cv2.rectangle(frame,(x_,y_),(x_+w_,y_+h_), (randrange(256), randrange(256), randrange(256)))
-    cv2.imshow('Smiley Face', frame)
-    cv2.waitKey(1)
-
-
+    # Converting Image to grayscale
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    # Classifiers Coordinates
+    faceCoordinates = faceClassifier.detectMultiScale(grayFrame)
+    smileCoordinates = smileClassifier.detectMultiScale(grayFrame, scaleFactor=1.7, minNeighbors=20)
+    print('Face Coordinates - ',faceCoordinates,'\nSmile Coordinates - ',smileCoordinates)
+    # Rectangles on the face
+    #for (x,y,w,h) in faceCoordinates:
+    faceFrame = [cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256), randrange(256), randrange(256))) for (x,y,w,h) in faceCoordinates]
+    smileFrame = [cv2.rectangle(frame, (x,y), (x+w,y+h), (randrange(256), randrange(256), randrange(256)),2) for (x,y,w,h) in smileCoordinates]
+    # Get the sub frame (using numpy N-dimensional array slicing)
+    #theFace = frame[y:y+h, x:x+w]
+    cv2.imshow('Webcam Smiley Face Detection',frame)
+    key = cv2.waitKey(1)
+    #Stop if Q is pressed
+    if key==81 or key==113:
+        break
+#Release the VideoCapture object
+print(faceCoordinates)
 video.release()
-cv2.destroyAllWindows()
-print('Code Completed')
